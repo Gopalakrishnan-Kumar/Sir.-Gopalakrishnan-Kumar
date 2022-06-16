6209e455ad6a86b14af9d895ed373c54256581e0
Gopalakrishnan Kumar <Gopalakrishnan-Kumar@users.noreply.github.com>
2022-06-16 05:46:56 +0530

    Add files via upload

--- File_1.R
+++ File_1.R
@@ -57,3 +57,8 @@ model_1 = lm(Best.Builders~ Shabari+Society.Requirement, data = df1)
 summary(model_1)
 plot(model_1)
 write.csv(df1,"submission_2.csv")
+
+model_1 = lm(Best.Builders~ Society.Requirement, data = df1)
+summary(model_1)
+plot(model_1)
+write.csv(df1,"submission_2.csv")
